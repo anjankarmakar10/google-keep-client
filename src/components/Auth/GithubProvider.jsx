bc9dce961fdcd2bdcab4f440d658274c2b49c1b6
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-06-12 12:20:33 +0600

    implement github login

--- src/components/Auth/GithubProvider.jsx
+++ src/components/Auth/GithubProvider.jsx
@@ -1,8 +1,26 @@
 import SignInButton from "./Provider.style";
+import { useAuth } from "../../context/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const GithubProvider = () => {
+  const { signInWithGitHub } = useAuth();
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  const from = state?.path || "/";
+
+  const handleSignIn = async () => {
+    try {
+      await signInWithGitHub();
+      navigate(from, { replace: true });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <SignInButton
+      onClick={handleSignIn}
       title={"Continue with Github"}
       img={
         "https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg"
